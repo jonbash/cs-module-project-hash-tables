ee3eaef5c2e95bddc0b109d0148670c227a52250
Jon Bash <jonbash@protonmail.com>
2020-06-25 19:55:23 -0700

    improve sumdiff

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -3,55 +3,47 @@ find all a, b, c, d in q such that
 f(a) + f(b) = f(c) - f(d)
 """
 
-q = set(range(1, 10))
+# q = set(range(1, 10))
 # q = set(range(1, 200))
 # q = (1, 3, 4, 7, 12)
-f_lookup = {}
-sum_f_lookup = {}
-diff_f_lookup = {}
-abcd_lookup = {}
+q = set(range(1, 50))
 
-def lookup(x, table, func):
-    result = table.get(x)
-    if result is None:
-        result = func(x)
-        table[x] = result
-    return result
+f_lookup = {}
+d_lookup = {}
 
 
 def f(x):
-    return lookup(x, f_lookup, lambda x: x * 4 + 6)
-
-
-def sum_f(x, y):
-    return lookup((x, y),
-                  sum_f_lookup,
-                  lambda x: f(x[0]) + f(x[1]))
-
-
-def diff_f(x, y):
-    return lookup((x, y),
-                  diff_f_lookup,
-                  lambda x: f(x[0]) - f(x[1]))
-
-
-def abcd_eq(a, b, c, d):
-    return lookup((a, b, c, d),
-                  abcd_lookup,
-                  lambda x: sum_f(x[0], x[1]) == diff_f(x[2], x[3]))
-
-
-def sumdiff(q):
-    output = []
-    for a in q:
-        for b in q:
-            for c in q:
-                for d in q:
-                    if abcd_eq(a, b, c, d):
-                        output.append((a, b, c, d))
+    output = f_lookup.get(x)
+    if output is None:
+        output = x * 4 + 6
+        f_lookup[x] = output
     return output
 
 
-test = sumdiff(q)
-for thing in test:
-    print(thing)
+def get_d(a, b, c):
+    output = d_lookup.get((a, b, c))
+    if output is None:
+        output = c - a - b - 3
+        f_lookup[(a, b, c)] = output
+        f_lookup[(b, a, c)] = output
+    return output
+
+
+def sumdiff(q):
+    # output = []
+    for a in q:
+        for b in q:
+            for c in q:
+                d = get_d(a, b, c)
+                if d in q:
+                    s = f"f({a}) + f({b}) = f({c}) - f({d})\t{f(a)} + {f(b)} = {f(c)} - {f(d)}"
+                    print(s)
+                    # output.append(s)
+    # return output
+
+
+# sumdiff(q)
+'''test = semdiff(q) '''
+sumdiff(q)
+# for thing in test:
+#     print(thing)
